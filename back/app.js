892b7bcca60fe960aa5363bf0022c038f2717a21
Flavia Oliveira <flaviacoliv@hotmail.com>
2026-01-30 17:26:28 -0300

    300126 6

--- back/app.js
+++ back/app.js
@@ -15,6 +15,38 @@ const dpConfig = {
 app.use(express.json());
 app.use(cors());
 
-app.post('/salvar-agendamento', async (req, res)->{
+app.post('/salvar-agendamento', async (req, res)=>{
+    const { nome, procedimento, dia, hora } = req.body;
+
+    let connection;
     
+    try {
+        connection = await mysql.createConnection(dbConfig);
+        console.log('Conectado ao Bando MySQL')
+
+        const insertQuery = '
+        INSERT INTO agendamentod (nome, procedimento, dia, hora)
+        VALUEA (?,?,?,?);
+        ';
+
+        const [result] = await connection.execute(insert)insertQuery, [nome, procedimento, dia, hora] 
+    } catch (err) {
+        console.error('Erro ao processar o agendamento: ', err);
+        res.status(500).send( { message: 'Erro interno ao salvar agendamento'});
+
+        console.log('Agendamento salvo com sucesso! ID:${result.insertId}');
+
+        const mensagem = 'Olá ${nome}, aguardamos você para realizar o seu procedimento ${dia} às ${hora} horas.'
+
+        res.status(200).send({message: mensagem});
+
+    }finally{
+        if(connection){
+            connection.end();
+        }
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log('Servidor rodando em http://localhost:${PORT}');
 })
